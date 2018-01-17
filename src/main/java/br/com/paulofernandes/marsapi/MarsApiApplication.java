cef57e87269b2bfb0722c98dfc7464db13c013d6
Paulo Fernandes <paulo.e.f.fernandes@gmail.com>
2018-01-17 19:17:23 -0200

    Commit inicial

---
+++ src/main/java/br/com/paulofernandes/marsapi/MarsApiApplication.java
@@ -0,0 +1,12 @@
+package br.com.paulofernandes.marsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsApiApplication.class, args);
+	}
+}
